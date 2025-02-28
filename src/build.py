971bbd29eea251f425db692c75999e5d3141e408
Lizzy <23005387+Cuteminded@users.noreply.github.com>
2025-02-28 22:14:55 +0100

    Update build.py

--- src/build.py
+++ src/build.py
@@ -1,7 +1,7 @@
 import time
 
 title = "Lizzys Blocklist"
-description = "A list of annoying spam domains"
+description = "A list of annoying spam domains to help users block unwanted content."
 homepage = "https://github.com/cuteminded/hostBlocklist"
 
 domains = []
@@ -21,9 +21,24 @@ def writeDomains():
 def readme():
     lines = []
     lines.append("# {0}".format(title))
+    lines.append(" ")
     lines.append("{0}<br>".format(description))
+    lines.append(" ")
+    lines.append("## Domains")
     lines.append("Number of Domains: {0}<br>".format(len(domains)))
     lines.append("Last modified: {0}<br>".format(time.strftime("%d-%m-%Y")))
+    lines.append(" ")
+    lines.append("## Usage")
+    lines.append(" ")
+    lines.append("You can use this blocklist to enhance your ad-blocker, firewall, or DNS filtering system. To integrate this list, follow these steps:")
+    lines.append("1. Download the latest blocklist from this repository.")
+    lines.append("2. Add the domains to your preferred blocking tool (e.g., Pi-hole, AdGuard, uBlock Origin, or custom DNS configurations).")
+    lines.append("3. Enjoy a cleaner browsing experience!")
+    lines.append(" ")
+    lines.append("## Contributing")
+    lines.append(" ")
+    lines.append("If you want to contribute to this blocklist, please submit a pull request with your changes. Your help is much appreciated!")
+
     with open("../README.md", "w") as file:
         for line in lines:
             file.write(f"{line}\n")
